95f150404b60d8c8a843400e1c8fca0f17bdf8b8
Sn00pyW00dst0ck <gabealdous@gmail.com>
2023-11-07 12:34:05 -0500

    Updating to pass lint in python 3 12

--- test/test_main.py
+++ test/test_main.py
@@ -3,7 +3,9 @@
 import unittest
 
 class HelloWorldTest(unittest.TestCase):
+    """A 'Hello World' test case to ensure the system is setup properly."""
     def test_hello_world(self):
+        """This dummy test should always pass. If it doesn't, then something is really wrong!"""
         self.assertEqual('Hello world!', 'Hello world!')
 
 if __name__ == '__main__':
